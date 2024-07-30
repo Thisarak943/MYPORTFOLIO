67e3fff08311468d7d93e5e51987bad6794741b3
Thisara Kandage <159875979+Thisarak943@users.noreply.github.com>
2024-07-30 17:09:46 +0530

    footer all done

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
-import "./Footer.css"
+import React from 'react';
+import './Footer.css';
+import { Link } from 'react-scroll';
+import { menu, socialHandles } from '../../data'; // Import socialHandles from your data file
 
-const Footer=() =>{
+const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer id="footer">
+      <div className="section_wrapper">
+        <ul className="nav_link_container">
+          {menu.map((list, index) => (
+            <Link
+              activeClass="active"
+              className="tab_item name nav_link"
+              to={list.name.toLowerCase()}
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              key={index}
+            >
+              {list.name}
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="social_handles_container">
+        {socialHandles.map((list, index) => (
+          <a href={list.link} className="icon_container social_handles social_handle" target="_blank" rel="noopener noreferrer" key={index}>
+            {list.icon}
+          </a>
+        ))}
+      </div>
+      <div className="copyright_container">
+        <h3>Copyright &copy; All right reserved | 2024</h3>
+        <p className="text_muted">Build by Thisara Kandage</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
-
+export default Footer;
