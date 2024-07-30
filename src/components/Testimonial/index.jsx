f727aec5c317bcd53256346189d73778a766f47f
Thisara Kandage <159875979+Thisarak943@users.noreply.github.com>
2024-07-30 14:18:00 +0530

    testomial js done

--- src/components/Testimonial/index.jsx
+++ src/components/Testimonial/index.jsx
@@ -1,13 +1,70 @@
-import React from 'react'
-import "./Testimonial.css"
+import React from 'react';
+import "./Testimonial.css";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { profile3, profile4, profile5, profile6 } from '../../images';
 
-const Testimonial=() =>{
+const data = [
+  {
+    avatar: profile3,
+    name: 'Samuel Eze',
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea`
+  },
+  {
+    avatar: profile4,
+    name: 'Emmanuel Joseph',
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea`
+  },
+  {
+    avatar: profile5,
+    name: 'Gloria Chiwendu',
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea`
+  },
+  {
+    avatar: profile6,
+    name: 'Precious Stone',
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea`
+  }
+];
+
+const Testimonial = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <section id='testimonial'>
+      <div className='section__wrapper'>
+        <div className="section__header">
+          <h2 className="primary__title">Testimonials</h2>
+          <p className="text_muted description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac consectetur quam.
+          </p>
+        </div>
+        <Swiper 
+          className='testimonial__container'
+          modules={[Pagination]}
+          spaceBetween={40}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          breakpoints={{
+            700: {
+              slidesPerView: 2,
+            }
+          }}
+        >
+          {
+            data.map(({ avatar, name, review }, index) => (
+              <SwiperSlide key={index}>
+                <div className='client__avatar'>
+                  <img src={avatar} alt={name} />
+                </div>
+                <small className="client__review">{review}</small>
+              </SwiperSlide>
+            ))
+          }
+        </Swiper>
+      </div>
+    </section>
+  );
 }
 
-export default Testimonial
-
+export default Testimonial;
